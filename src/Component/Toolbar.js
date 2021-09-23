4e4db1a33cef610d5d18a6c61d7423c26515ff13
Johnk Espitia <jcespitia1@gmail.com>
2021-09-23 17:41:21 -0500

    Adding Styled-components and Scss (Sass)

--- src/Component/Toolbar.js
+++ src/Component/Toolbar.js
@@ -1,43 +1,25 @@
 import React, { useState, useEffect } from 'react'
 import {Navbar, Container, Nav} from 'react-bootstrap'
 import { Link } from 'react-router-dom'
-/*
-class Toolbar extends React.Component{
-    constructor(props) {
-        super(props);
-        this.state = {
-            date: new Date(), 
-            name:""
-        };
-
-    }
-    componentDidMount() {
-        console.log("Mounted")
-    }
-    
-      componentWillUnmount() {
-        console.log("will Unmount")
-      }
-      componentDidUpdate() {
-          console.log("Component updated!")
-      }
-    render(){
-        return <h1 style={{
-            color: this.props.color
-        }}>Hello World {this.state.date.toDateString()} <input type="text" onChange={(e)=>{
-            this.setState({
-                ...this.state,
-                name: e.target.value
-            })
-        }} /></h1>
-        
-    }
-}
-*/
-
+import styled from 'styled-components'
+import styles from './Toolbar.module.scss'
+//0 - CSS 
 //1 - Styled Components
 //2 - UI Library (Material, Bootstrap)
 //3 - Sass (scss)
+const defaultColor = "#141925";
+const primaryColor = "#ffa800";
+
+const NavNew = styled(Nav)`
+background-color: ${primaryColor};
+@media (max-width: "767px") {
+    margin-top: 20px;
+    width: 100%;
+    justify-content: center;
+  }
+
+`
+
 const Toolbar = ({color}) => {
     const [date,] = useState(new Date())
     const [name, setName] = useState("Johnk") //flux 
@@ -48,11 +30,11 @@ const Toolbar = ({color}) => {
     return <Navbar bg={"dark"} variant={"dark"} fluid={true}>
         <Container fluid={true}>
             <Navbar.Brand as={Link} to="/">Demo App</Navbar.Brand>
-            <Nav>
-                <Nav.Link as={Link} to="/">Home</Nav.Link>
+            <NavNew>
+                <Nav.Link className={styles.linkHome} as={Link} to="/">Home</Nav.Link>
                 <Nav.Link as={Link} to="/user/1">User</Nav.Link>
                 <Nav.Link as={Link} to="/Login">Login</Nav.Link>
-            </Nav>
+            </NavNew>
         </Container>
     </Navbar>
 }
