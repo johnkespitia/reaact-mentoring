4be91b6079c0a08eba3241424709ee33d04a919b
Johnk Espitia <jcespitia1@gmail.com>
2021-09-23 17:19:21 -0500

    Implement react-bootstrap

--- src/Component/Toolbar.js
+++ src/Component/Toolbar.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react'
+import {Navbar, Container, Nav} from 'react-bootstrap'
 import { Link } from 'react-router-dom'
 /*
 class Toolbar extends React.Component{
@@ -34,6 +35,9 @@ class Toolbar extends React.Component{
 }
 */
 
+//1 - Styled Components
+//2 - UI Library (Material, Bootstrap)
+//3 - Sass (scss)
 const Toolbar = ({color}) => {
     const [date,] = useState(new Date())
     const [name, setName] = useState("Johnk") //flux 
@@ -41,23 +45,16 @@ const Toolbar = ({color}) => {
     const setNameReal = (e) => {
         setName(e.target.value)
     }
-    return <div style={{
-        backgroundColor: color
-    }}>
-        <nav>
-            <ul>
-                <li>
-                    <Link to="/">Home</Link>
-                </li>
-                <li>
-                    <Link to="/user">User</Link>
-                </li>
-                <li>
-                    <Link to="/login">Login</Link>
-                </li>
-            </ul>
-        </nav> 
-    </div>
+    return <Navbar bg={"dark"} variant={"dark"} fluid={true}>
+        <Container fluid={true}>
+            <Navbar.Brand as={Link} to="/">Demo App</Navbar.Brand>
+            <Nav>
+                <Nav.Link as={Link} to="/">Home</Nav.Link>
+                <Nav.Link as={Link} to="/user/1">User</Nav.Link>
+                <Nav.Link as={Link} to="/Login">Login</Nav.Link>
+            </Nav>
+        </Container>
+    </Navbar>
 }
 
 export default Toolbar
--- src/Screens/Home/index.js
+++ src/Screens/Home/index.js
@@ -1,8 +1,11 @@
 import React from 'react'
+import {Button} from 'react-bootstrap'
 
 const Home = (props) => {
     return <div>
         <h1>Home</h1>
+        <button className="btn btn-success">Go!</button>
+        <Button variant={"info"}>Go 2</Button>
     </div>
 }
 
