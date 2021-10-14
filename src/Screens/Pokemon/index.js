34ead34292e14394ab7384dc5486248b501d92f5
Johnk Espitia <jcespitia1@gmail.com>
2021-10-14 17:46:46 -0500

    Changing fetch to graphql

--- src/Screens/Pokemon/index.js
+++ src/Screens/Pokemon/index.js
@@ -1,26 +1,51 @@
 
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
+import { gql, useQuery} from '@apollo/client'
 import SearchComponent from './SearchComponent'
 const PokemonScreen = () => {
     const [pokemon, setPokemon] = useState({})
-    const getPokemon = async (nameToSearch) =>{
-        await fetch(`https://pokeapi.co/api/v2/pokemon/${nameToSearch}`, {
+    const [pokemonName, setPokemonName] = useState("bulbasaur")
+    const GetPokemon = async (nameToSearch) =>{
+        /*await fetch(`https://pokeapi.co/api/v2/pokemon/${nameToSearch}`, {
             method:'GET', 
         }).then((response)=> response.json()).then(data=>{
             setPokemon(data)
-        })        
+        })  
+        */
+        setPokemonName(nameToSearch)
+    }
+    const query = gql`
+    query samplePokeAPIquery {
+        pokemon_v2_pokemon(where: {name: {_eq: "${pokemonName}"}}) {
+          name
+          height
+          pokemon_v2_pokemontypes {
+            pokemon_v2_type {
+              name
+            }
+          }
+          pokemon_v2_pokemonsprites {
+            sprites
+          }
+        }
+      }
+    `
+    const result = useQuery(query)
+    console.log(result.data?.pokemon_v2_pokemon[0].name)
+    if(pokemon == null || (Object.keys(pokemon).length === 0 || pokemon.name != pokemonName)){
+        setPokemon(result.data?.pokemon_v2_pokemon[0])
     }
 
     return <div>
-        <SearchComponent searchFunction={getPokemon} />
-        <div>
+        <SearchComponent searchFunction={GetPokemon} />
+        {pokemon != null && Object.keys(pokemon).length > 0 && <div>
             <h1>{pokemon.name}</h1>
             <img src={pokemon?.sprites?.front_default} alt={pokemon.name} />
             <ul>
                 <li>Weight: {pokemon.weight}</li>
                 <li>Width: {pokemon.height}</li>
             </ul>
-        </div>
+        </div>}
     </div>
 } 
 
--- src/index.js
+++ src/index.js
@@ -1,14 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux'
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+} from '@apollo/client'
 import App from './App';
 import store from './Redux/store'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './index.css';
 
+const client = new ApolloClient({
+  uri: 'https://beta.pokeapi.co/graphql/v1beta',
+  cache: new InMemoryCache()
+});
+
 ReactDOM.render(
     <Provider store={store}>
-      <App />
+      <ApolloProvider client={client}>
+        <App/>
+      </ApolloProvider>
     </Provider>,
   document.getElementById('root')
 );
